d36a836fb8ff997ee693b5e2a4ce5839a0b1eff2
sadig muradov <sadmur2002@gmail.com>
2022-09-22 23:35:34 +0400

    master: initial changes

---
+++ src/main/java/com/sm/springreactivemongocrud/repo/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.sm.springreactivemongocrud.repo;
+
+import com.sm.springreactivemongocrud.dto.ProductDto;
+import com.sm.springreactivemongocrud.entity.Product;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
+
+    Flux<ProductDto> findByPriceBetween(Range<Double> priceRange);
+}
---
+++ src/main/java/com/sm/springreactivemongocrud/utils/AppUtils.java
@@ -0,0 +1,22 @@
+package com.sm.springreactivemongocrud.utils;
+
+import com.sm.springreactivemongocrud.dto.ProductDto;
+import com.sm.springreactivemongocrud.entity.Product;
+import org.springframework.beans.BeanUtils;
+
+public class AppUtils {
+
+    public static ProductDto entityToDto(Product product) {
+        ProductDto productDto = new ProductDto();
+        BeanUtils.copyProperties(product, productDto);
+
+        return productDto;
+    }
+
+    public static Product dtoToEntity(ProductDto productDto) {
+        Product product = new Product();
+        BeanUtils.copyProperties(productDto, product);
+
+        return product;
+    }
+}
